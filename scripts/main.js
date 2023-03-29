449bec18b812e928cb592fdcb2624e3f9974bd3a
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-03-29 04:13:29 +0300

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,65 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const inputLoginUsername = document.getElementById('user-id');
+const inputLoginPassword = document.getElementById('user-password');
+
+const labelWelcome = document.querySelector('.welcome');
+
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance-value');
+const labelSumIn = document.querySelector('.summary-value--in');
+const labelSumOut = document.querySelector('.summary-value--out');
+const labelSumInterest = document.querySelector('.summary-value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnTransfer = document.querySelector('.form-btn--transfer');
+const btnLoan = document.querySelector('.form-btn--loan');
+const btnClose = document.querySelector('.form-btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputTransferTo = document.querySelector('.form-input--to');
+const inputTransferAmount = document.querySelector('.form-input--amount');
+const inputLoanAmount = document.querySelector('.form-input--loan-amount');
+const inputCloseUsername = document.querySelector('.form-input--user');
+const inputClosePassword = document.querySelector('.form-input--password');
+
+//Note: Data
+
+const account1 = {
+  owner: 'Violet Summers',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  password: 'ArrayOfSunshine',
+};
+
+const account2 = {
+  owner: 'Cyan Thompson',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  password: 'JSBankVault!',
+};
+
+const account3 = {
+  owner: 'Amber Lynn Rayleigh',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  password: 'SassMaster2000',
+};
+
+const account4 = {
+  owner: 'Indigo Martinez',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  password: 'MethodToTheMadness',
+};
+
+const accounts = [account1, account2, account3, account4];
 
 // Function to open the modal window and display the overlay
 const openModal = function () {
@@ -21,7 +79,7 @@ const closeModal = function () {
 };
 
 // Add click event listeners to all open buttons to open the modal window
-btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+btnOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
 
 // Add click event listener to close button to close the modal window
 btnCloseModal.addEventListener('click', closeModal);
@@ -37,81 +95,36 @@ document.addEventListener('keydown', function (e) {
 
 //Note: change to banking view
 
+let currentAccount;
+
 document.getElementById('login-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  // Get user inpurt
-  const userID = document.getElementById('user-id').value;
-  const userPassword = document.getElementById('user-password').value;
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
 
-  // Validate user credentials
-  if (userID === 'test' && userPassword === 'test') {
-    // Hide marketing content and show account information
-    document.getElementById('marketing-content').style.display = 'none';
-    document.getElementById('account-info').style.display = 'grid';
+  if (currentAccount) {
+    if (currentAccount.password === inputLoginPassword.value) {
+      closeModal();
+      // Hide marketing content and show account information
+      document.getElementById('marketing-content').style.display = 'none';
+      document.getElementById('account-info').style.display = 'grid';
+      labelWelcome.textContent = `Welcome back, ${currentAccount.owner}`;
+      displayMovements(currentAccount.movements);
+        calcDisplayBalance(currentAccount.movements);
+        calcDisplaySummary(currentAccount.movements);
+    } else {
+      // Show an error message for incorrect password
+      alert('Invalid password. Please try again!');
+    }
   } else {
-    // Show an error message or handle invalid credentials
-    alert('Invalid ID or password. Please try again');
+    // Show an error message for non-existing or incorrect username
+    alert(
+      'Invalid username. Please check the documentation on GitHub for the correct username and password'
+    );
   }
 });
-
-//Note: Data
-
-const account1 = {
-  owner: 'Violet Summers',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Cyan Thompson',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Amber Rayleigh',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Indigo Martinez',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -136,20 +149,41 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account2.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    const names = acc.owner.toLowerCase().split(' ');
+    const firstNameInitial = names[0][0];
+    const lastName = names[names.length - 1];
+    // const randomNumber = Math.floor(Math.random() * 1000);
+    acc.username = firstNameInitial + lastName;
+  });
+};
+createUsernames(accounts);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${Math.abs(interest)}€`;
+};
+
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const eurToUsd = 1.1;
-
-const movementsUSD = movements.map((mov) => mov * eurToUsd);
-console.log(movements);
-console.log(movementsUSD);
-
-const movementsDescription = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${
-      mov > 0 ? 'deposited' : 'withdrew'
-    } withdrew ${Math.abs(mov)}`
-);
-console.log(movementsDescription);
