8fc12479f86b20c30835e4f52ca4f844ce33f3ff
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-03-31 21:47:05 +0300

    Clean up

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,5 +1,25 @@
 //Note: functionality of the site unrelated to the bank movements
 
+// Create an Intersection Observer object
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const lazyImage = entry.target;
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.classList.remove('lazy');
+      observer.unobserve(lazyImage);
+    }
+  });
+});
+
+// Select the images to be lazy loaded and register them with the Intersection Observer object
+const lazyImages = document.querySelectorAll('.lazy');
+lazyImages.forEach((lazyImage) => {
+  observer.observe(lazyImage);
+});
+
+
+
 //Get the mobile navbar
 const navToggle = document.querySelector('.mobile-nav-toggle');
 // Event listener for navigation toggle from hamburger menu to normal and vice versa
