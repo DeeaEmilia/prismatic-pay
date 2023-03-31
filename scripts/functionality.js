1b8d654e3530e8eac05a71d363432d6042663923
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-03-31 22:42:20 +0300

    added functionality

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -18,7 +18,25 @@ lazyImages.forEach((lazyImage) => {
   observer.observe(lazyImage);
 });
 
+// Get all the operations tabs and content
+const tabs = document.querySelectorAll('.operations-tab');
+const contents = document.querySelectorAll('.operations-content');
 
+// Event listener to each tab button
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    // Get the data-tab attribute of the clicked tab button
+    const tabId = tab.dataset.tab;
+
+    // Remove the active class from all tabs and contents
+    tabs.forEach((tab) => tab.classList.remove('operations-tab--active'));
+    contents.forEach((content) => content.classList.remove('operations-content--active'));
+
+    // Add the active class to the clicked tab and content
+    tab.classList.add('operations-tab--active');
+    document.querySelector(`.operations-content--${tabId}`).classList.add('operations-content--active');
+  });
+});
 
 //Get the mobile navbar
 const navToggle = document.querySelector('.mobile-nav-toggle');
@@ -35,4 +53,74 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
     console.log(visibility);
+});
+
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelector('.dots');
+const dotButtons = [];
+
+// Create a button for each slide
+slides.forEach((slide, index) => {
+    const dotButton = document.createElement('button');
+    dotButton.classList.add('dot');
+    dots.appendChild(dotButton);
+    dotButtons.push(dotButton);
+});
+
+let currentSlide = 0;
+
+// Set the first slide as active
+slides[currentSlide].classList.add('slide--active');
+dotButtons[currentSlide].classList.add('dot--active');
+
+// Move to the next slide
+function nextSlide() {
+    // Remove the active class from the current slide and dot
+    slides[currentSlide].classList.remove('slide--active');
+    dotButtons[currentSlide].classList.remove('dot--active');
+
+    // Move to the next slide and wrap around if at the end
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    // Add the active class to the new slide and dot
+    slides[currentSlide].classList.add('slide--active');
+    dotButtons[currentSlide].classList.add('dot--active');
+}
+
+// Move to the previous slide
+function prevSlide() {
+    // Remove the active class from the current slide and dot
+    slides[currentSlide].classList.remove('slide--active');
+    dotButtons[currentSlide].classList.remove('dot--active');
+
+    // Move to the previous slide and wrap around if at the beginning
+    currentSlide = (currentSlide + slides.length - 1) % slides.length;
+
+    // Add the active class to the new slide and dot
+    slides[currentSlide].classList.add('slide--active');
+    dotButtons[currentSlide].classList.add('dot--active');
+}
+
+// Add click event listeners to the arrow buttons
+const leftButton = document.querySelector('.slider-btn--left');
+leftButton.addEventListener('click', prevSlide);
+
+const rightButton = document.querySelector('.slider-btn--right');
+rightButton.addEventListener('click', nextSlide);
+
+// Add click event listeners to the dots
+dotButtons.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        // Move to the clicked slide
+        if (index !== currentSlide) {
+            // Remove the active class from the current slide and dot
+            slides[currentSlide].classList.remove('slide--active');
+            dotButtons[currentSlide].classList.remove('dot--active');
+
+            // Move to the clicked slide and add the active class
+            currentSlide = index;
+            slides[currentSlide].classList.add('slide--active');
+            dotButtons[currentSlide].classList.add('dot--active');
+        }
+    });
 });
